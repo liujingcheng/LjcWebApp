908c3ab52efc14df9098fc3cac14cd4332021d90
liujingcheng <cyliujingcheng@126.com>
2016-07-28 23:27:17 +0800

    初步完成MySqlEF的引进及表实体的加入

---
+++ src/LjcWebApp/Models/MyContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LjcWebApp.Models
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<configmemorycurve_tb>();
+            modelBuilder.Entity<introspect>();
+            modelBuilder.Entity<question>();
+            modelBuilder.Entity<timestatistic>();
+            modelBuilder.Entity<word_tb>();
+        }
+
+        public virtual DbSet<configmemorycurve_tb> Configmemorycurve { get; set; }
+        public virtual DbSet<introspect> Introspect { get; set; }
+        public virtual DbSet<question> Question { get; set; }
+        public virtual DbSet<timestatistic> Timestatistic { get; set; }
+        public virtual DbSet<word_tb> Word { get; set; }
+    }
+}
