a2f721a8819a940892917a72c2cf36a7caa67b63
liujingcheng <cyliujingcheng@126.com>
2016-08-02 11:19:10 +0800

    粗略完成复制功能

--- src/LjcWebApp/wwwroot/js/bundle.js
+++ src/LjcWebApp/wwwroot/js/bundle.js
@@ -2668,22 +2668,6 @@ $(document).ready(function () {
     InitZeroClip();
 });
 
-function InitZeroClip () {
-
-    var client = new ZeroClipboard($('.copy-button'), { moviePath: "~/lib/zeroclipboard/dist/ZeroClipboard.swf" }, { forceHandCursor: true });
-
-    client.on('datarequested', function () {
-        client.setText($(this).parent().attr('copyText').replace(new RegExp("/", "gm"), "\\"));
-    });
-
-    client.on('complete', function () {
-        $(this).parent().popover("show");
-        setTimeout(function () { $('.popover').hide(); }, 1000);
-    });
-
-    client.on('wrongflash noflash', function () {
-        //"当前浏览器不支持'复制'按钮的功能,可能因为未安装flash或者安装的flash版本不支持该功能"
-        ZeroClipboard.destroy();
-        $(".copy-button-wrap ").remove();
-    });
+function InitZeroClip() {
+    var client = new ZeroClipboard($('.clip_button'));
 }
