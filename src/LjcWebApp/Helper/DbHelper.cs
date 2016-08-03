cf3475b9dfc40378ad7f30d67049e40e71c01c0f
liujingcheng <cyliujingcheng@126.com>
2016-08-03 23:32:28 +0800

    去掉连接信息

--- src/LjcWebApp/Helper/DbHelper.cs
+++ src/LjcWebApp/Helper/DbHelper.cs
@@ -8,7 +8,7 @@ namespace LjcWebApp.Helper
 {
     public static class DbHelper
     {
-        public const string DbConnectionString = "server=rdsfarfbifarfbi.mysql.rds.aliyuncs.com;database=wordtest;uid=aspuser;pwd=ljc1qazse4";
+        public const string DbConnectionString = "";
         public static DbContextOptionsBuilder Builder = new DbContextOptionsBuilder();
     }
 }
